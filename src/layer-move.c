2988d6649a8f7105ffa8a23ff04d0ed08c72a682
kbys <work4kbys@gmail.com>
2020-11-24 02:42:12 +0900

    update layer-move

--- src/layer-move.c
+++ src/layer-move.c
@@ -7,19 +7,20 @@ struct event_context{
   BiLayer *layer;
 };
 
-static BiNode* create_new_node(int x, int y,BiTextureImage *img)
+static BiNode* create_new_node(int x, int y,BiTexture *tex)
 {
     BiNode* node = malloc(sizeof(BiNode));
     bi_node_init(node);
 
     // texture
-    node->texture = malloc(sizeof(BiTexture));
-    node->texture->texture_image = img;
+    node->texture_mapping = malloc(sizeof(BiTextureMapping));
+    bi_texture_mapping_init(node->texture_mapping);
+    node->texture_mapping->texture = tex;
     int tw = 32;
     int th = 32;
-    int tx = rand()%(img->w/tw) * tw;
-    int ty = rand()%(img->h/th) * th;
-    bi_set_texture_boundary(node->texture,tx,ty,tw,th);
+    int tx = rand()%(tex->w/tw) * tw;
+    int ty = rand()%(tex->h/th) * th;
+    bi_texture_mapping_set_bound(node->texture_mapping,tx,ty,tw,th);
     bi_node_set_position(node,x,y);
     bi_node_set_size(node,tw,th);
     bi_set_color( node->color, 0xFF, 0xFF, 0xFF, 0xFF);
@@ -54,12 +55,15 @@ static void world_create(BiContext* context)
     BiLayer *layer = malloc(sizeof(BiLayer));
     bi_layer_init(layer);
     layer->projection_centering = true;
+    layer->z_order = -1;
+
     bi_add_layer(context,layer);
 
     // texture
-    BiTextureImage *img = malloc(sizeof(BiTextureImage));
-    bi_load_texture("assets/tile.png",img,false);
-    layer->textures[0] = img;
+    BiTexture *tex = malloc(sizeof(BiTexture));
+    bi_texture_init(tex);
+    bi_texture_load_from_file(tex,"assets/tile.png",false);
+    layer->textures[0] = tex;
 
     // root node
     BiNode* tiles = malloc(sizeof(BiNode));
@@ -78,16 +82,10 @@ static void world_create(BiContext* context)
 
     for(int x=0; x<64; x++) {
       for(int y=0; y<64; y++){
-        BiNode* node = create_new_node(x*32,y*32,img);
+        BiNode* node = create_new_node(x*32,y*32,tex);
         bi_add_node(tiles,node);
       }
     }
-
-    //
-    // fps layer
-    //
-    BiFontAtlas *font = load_font();
-    add_fps_layer(context,font);
 }
 
 int main(int argc,char* argv[])
@@ -95,6 +93,13 @@ int main(int argc,char* argv[])
     print_version();
     BiContext* context = malloc(sizeof(BiContext));
     bi_init_context(context, 480, 320, 0, false, __FILE__);
+
+    //
+    // fps layer
+    //
+    BiFontAtlas *font = load_font();
+    add_fps_layer(context,font);
+
     world_create(context);
     bi_start_run_loop(context);
     return 0;
