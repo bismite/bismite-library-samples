2177696ed81cc63e1ba18d6bb21d5768f8c41144
kbys <work4kbys@gmail.com>
2021-04-20 18:49:32 +0900

    use prebuilt SDL libraries in macOS

--- scripts/build.macos.sh
+++ scripts/build.macos.sh
@@ -1,13 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 TARGET="macos"
 DIR="build/$TARGET"
 SOURCES=`find src -name "*.c"`
 
 CC="/usr/bin/clang"
-FRAMEWORKS="-F $HOME/Library/Frameworks -framework OpenGL -framework SDL2 -framework SDL2_image"
-CFLAGS="-Wall -Os -I $DIR/include -I $DIR/include/SDL2 -arch x86_64"
-LIBS="-L $DIR/lib -lbismite-ext -lbismite-core"
+ARCHFLAG="-arch x86_64 -arch arm64"
+FRAMEWORKS="-framework OpenGL"
+CFLAGS="-Wall -Os -I $DIR/include -I $DIR/include/SDL2 ${ARCHFLAG}"
+LIBS="-L $DIR/lib -lbismite-ext -lbismite-core -lSDL2 -lSDL2_image -lSDL2_mixer"
 
 BI_CORE_DIR=$DIR/bismite-library-core
 BI_EXT_DIR=$DIR/bismite-library-ext
@@ -16,15 +17,16 @@ mkdir -p $DIR/bin
 mkdir -p $DIR/lib
 mkdir -p $DIR/include
 
-sh ./scripts/$TARGET/install_sdl.sh $DIR
+./scripts/$TARGET/install_sdl.sh $DIR
+
 sh ./scripts/copy-bismite-libraries.sh $DIR
 
-(cd $BI_CORE_DIR; make -f Makefile.$TARGET.mk all INCLUDE_PATHS="-I ../include -I ../include/SDL2" CFLAGS="-Wall -Os -arch x86_64" )
+(cd $BI_CORE_DIR; make -f Makefile.$TARGET.mk all INCLUDE_PATHS="-I ../include -I ../include/SDL2" CFLAGS="-Wall -Os ${ARCHFLAG}" )
 if [ $? != 0 ]; then exit 1; fi
 cp $BI_CORE_DIR/build/$TARGET/*.a $DIR/lib/
 cp -r $BI_CORE_DIR/include/bi $DIR/include/
 
-(cd $BI_EXT_DIR; make -f Makefile.$TARGET.mk all INCLUDE_PATHS="-I ../include -I ../include/SDL2"  CFLAGS="-Wall -Os -arch x86_64" )
+(cd $BI_EXT_DIR; make -f Makefile.$TARGET.mk all INCLUDE_PATHS="-I ../include -I ../include/SDL2"  CFLAGS="-Wall -Os ${ARCHFLAG}" )
 if [ $? != 0 ]; then exit 1; fi
 cp $BI_EXT_DIR/build/$TARGET/*.a $DIR/lib/
 cp -r $BI_EXT_DIR/include/bi $DIR/include/
@@ -32,11 +34,11 @@ cp -r $BI_EXT_DIR/include/bi $DIR/include/
 #
 # build samples
 #
-
 for SRC in $SOURCES; do
   echo $SRC
   NAME=`basename $SRC .c`
   $CC -Wall -c $SRC -o $DIR/bin/$NAME.o $CFLAGS
-  $CC -Wall $DIR/bin/$NAME.o -o $DIR/bin/$NAME $LIBS $FRAMEWORKS -arch x86_64
+  $CC -Wall $DIR/bin/$NAME.o -o $DIR/bin/$NAME $LIBS $FRAMEWORKS $ARCHFLAG
+  install_name_tool -add_rpath @executable_path/../lib $DIR/bin/$NAME
   if [ $? != 0 ]; then exit 1; fi
 done
--- scripts/macos/install_sdl.sh
+++ scripts/macos/install_sdl.sh
@@ -1,54 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-DL_DIR=$1
+mkdir -p build/download
 
-mkdir -p $DL_DIR
+URL=https://github.com/bismite/SDL-macOS-UniversalBinaries/releases/download/main/SDL-macOS-UniversalBinaries.tgz
+SDL_TGZ=SDL-macOS-UniversalBinaries.tgz
 
-FRAMEWORKS_DIR=$HOME/Library/Frameworks
-mkdir -p $FRAMEWORKS_DIR
-
-
-# SDL2
-SDL2_FRAMEWORK=SDL2.framework
-SDL2_DMG=SDL2-2.0.10.dmg
-if [ ! -e $FRAMEWORKS_DIR/$SDL2_FRAMEWORK ]; then
-  echo "Install $SDL2_FRAMEWORK to $FRAMEWORKS_DIR"
-  if [ ! -e $DL_DIR/$SDL2_DMG ]; then
-    curl --progress-bar -S -L -o $DL_DIR/$SDL2_DMG https://www.libsdl.org/release/$SDL2_DMG
-  fi
-  hdiutil attach $DL_DIR/$SDL2_DMG
-  cp -R /Volumes/SDL2/$SDL2_FRAMEWORK $FRAMEWORKS_DIR
-  hdiutil detach /Volumes/SDL2
+if [ ! -e build/download/${SDL_TGZ} ]; then
+  curl --progress-bar -S -L -o build/download/${SDL_TGZ} ${URL}
 fi
+tar zxf build/download/${SDL_TGZ} -C build/macos
 
-# SDL2_image
-SDL2_IMAGE_FRAMEWORK=SDL2_image.framework
-SDL2_IMAGE_DMG=SDL2_image-2.0.5.dmg
-if [ ! -e $FRAMEWORKS_DIR/$SDL2_IMAGE_FRAMEWORK ]; then
-  echo "Install $SDL2_IMAGE_FRAMEWORK to $FRAMEWORKS_DIR"
-  if [ ! -e $DL_DIR/$SDL2_IMAGE_DMG ]; then
-    curl --progress-bar -S -L -o $DL_DIR/$SDL2_IMAGE_DMG https://www.libsdl.org/projects/SDL_image/release/$SDL2_IMAGE_DMG
-  fi
-  hdiutil attach $DL_DIR/$SDL2_IMAGE_DMG
-  cp -R /Volumes/SDL2_image/$SDL2_IMAGE_FRAMEWORK $FRAMEWORKS_DIR
-  hdiutil detach /Volumes/SDL2_image
-fi
-
-#SDL2_mixer
-SDL2_MIXER_FRAMEWORK=SDL2_mixer.framework
-SDL2_MIXER_DMG=SDL2_mixer-2.0.4.dmg
-if [ ! -e $FRAMEWORKS_DIR/$SDL2_MIXER_FRAMEWORK ]; then
-  echo "Install $SDL2_MIXER_FRAMEWORK to $FRAMEWORKS_DIR"
-  if [ ! -e $DL_DIR/$SDL2_MIXER_DMG ]; then
-    curl --progress-bar -S -L -o $DL_DIR/$SDL2_MIXER_DMG https://www.libsdl.org/projects/SDL_mixer/release/$SDL2_MIXER_DMG
-  fi
-  hdiutil attach $DL_DIR/$SDL2_MIXER_DMG
-  cp -R /Volumes/SDL2_mixer/$SDL2_MIXER_FRAMEWORK $FRAMEWORKS_DIR
-  hdiutil detach /Volumes/SDL2_mixer
-fi
-
-# header
-mkdir -p build/macos/include/SDL2
-cp -R ${FRAMEWORKS_DIR}/${SDL2_FRAMEWORK}/Headers/* build/macos/include/SDL2/
-cp -R ${FRAMEWORKS_DIR}/${SDL2_IMAGE_FRAMEWORK}/Headers/* build/macos/include/SDL2/
-cp -R ${FRAMEWORKS_DIR}/${SDL2_MIXER_FRAMEWORK}/Headers/* build/macos/include/SDL2/
+cp -R build/macos/SDL-macOS-UniversalBinaries/* build/macos/
